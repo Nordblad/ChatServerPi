0e794a4ac80c364f7cf398a5a74b60f65212ddde
Olov Nordblad <olle.nordblad@gmail.com>
2016-02-03 19:06:40 +0100

    Ändrat broadcast & servernamn

--- ChatServerPi/ChatServerPi/Program.cs
+++ ChatServerPi/ChatServerPi/Program.cs
@@ -11,13 +11,15 @@ namespace ChatServerPi
 {
     class Program
     {
-        private static List<ChatUser> clientList = new List<ChatUser>();
+        private static List<ChatClient> clientList = new List<ChatClient>();
+        private static string serverName = null;
 
         static void Main(string[] args)
         {
+            serverName = args.Length > 0 ? args[0] : "Default server name";
             TcpListener listener = new TcpListener(IPAddress.Any, 5000);
             listener.Start();
-            Console.WriteLine("Server up!");
+            Console.WriteLine("Server '" + serverName +  "'up and running!");
 
             while (true)
             {
@@ -28,9 +30,10 @@ namespace ChatServerPi
                 byte[] recievedData = new byte[512];
                 stream.Read(recievedData, 0, recievedData.Length);
                 //Spara den nyuppkopplade användaren
-                ChatUser user = new ChatUser { UserName = Encoding.Unicode.GetString(recievedData).TrimEnd('\0'), Client = client };
+                ChatClient user = new ChatClient { UserName = Encoding.Unicode.GetString(recievedData).TrimEnd('\0'), Client = client };
                 clientList.Add(user);
                 Console.WriteLine("*** User " + user.UserName + " joined. ***");
+                SendMessage(user.Client, "", "Välkommen till " + serverName + ". Det finns " + (clientList.Count-1) + " andra användare här.");
                 Broadcast("", user.UserName + " joined the chat.");
                 user.StartListening();
             }
@@ -39,26 +42,48 @@ namespace ChatServerPi
 
         internal static void Broadcast (string userName, string message)
         {
-            byte[] responseMessage = Encoding.Unicode.GetBytes(userName + "$" + message);
-            //FELSÖKNINGFELSÖKNINGFELSÖKNING
-            Console.WriteLine("Broadcasting to users: " + clientList.Count);
-            foreach (ChatUser client in clientList)
+            //byte[] responseMessage = Encoding.Unicode.GetBytes(userName + "$" + message);
+            ////FELSÖKNINGFELSÖKNINGFELSÖKNING
+            //Console.WriteLine("Broadcasting to users: " + clientList.Count);
+            //foreach (ChatUser client in clientList)
+            //{
+            //    try
+            //    {
+            //        NetworkStream stream = client.Client.GetStream();
+            //        stream.Write(responseMessage, 0, responseMessage.Length);
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("ERROR VID BROADCAST! " + client.UserName + ", " + clientList.Count);
+            //        Console.WriteLine("Message:[" + message + "], user: [" + userName + "]");
+            //    }
+            //}
+            foreach (ChatClient user in clientList)
             {
-                try
-                {
-                    NetworkStream stream = client.Client.GetStream();
-                    stream.Write(responseMessage, 0, responseMessage.Length);
-                }
-                catch
-                {
-                    Console.WriteLine("ERROR VID BROADCAST! " + client.UserName + ", " + clientList.Count);
-                    Console.WriteLine("Message:[" + message + "], user: [" + userName + "]");
-                }
-
+                SendMessage(user.Client, userName, message);
             }
         }
 
-        internal static void DisconnectUser (ChatUser user)
+        internal static void SendMessage (TcpClient user, string userName, string message)
+        {
+            if (user == null || !user.Connected)
+            {
+                Console.WriteLine("TRIED SENDING TO OFFLINE CLIENT!!");
+                return;
+            }
+            try
+            {
+                byte[] byteMessage = Encoding.Unicode.GetBytes(userName + "$" + message);
+                NetworkStream stream = user.GetStream();
+                stream.Write(byteMessage, 0, byteMessage.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR IN SENDING! " + e.Message);
+            }
+        }
+
+        internal static void DisconnectUser (ChatClient user)
         {
             Console.WriteLine("*** Client " + user.UserName + " disconnected. ***");
             user.Client.Close();
@@ -69,10 +94,12 @@ namespace ChatServerPi
             Console.WriteLine("Tre: users: " + clientList.Count);
             Broadcast("", user.UserName + " has left the chat.");
             //user.ListenThread.Abort();
-            user = null;
+
+            //PLACERA OM!
+            //user = null;
         }
     }
-    class ChatUser
+    class ChatClient
     {
         public string UserName { get; set; }
         public TcpClient Client { get; set; }
